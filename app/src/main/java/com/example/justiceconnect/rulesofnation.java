3eb72f2375fd97525f2d0d71e146c1f3cbca170d
Akshay peherkar <95123298+Akshayp02@users.noreply.github.com>
2024-01-20 15:00:55 +0530

    initial commit

---
+++ app/src/main/java/com/example/justiceconnect/rulesofnation.java
@@ -0,0 +1,14 @@
+package com.example.justiceconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rulesofnation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rulesofnation);
+    }
+}
